42d620ebcbd288c22fcc96f281e73dae7a3b6e7b
Alexander Vieth <vieth.alexander@gmx.net>
2026-07-14 15:14:45 +0200

    Restructure test

--- test/main.cpp
+++ test/main.cpp
@@ -39,50 +39,6 @@ namespace jsoncons {
 
 using namespace jsoncons;
 
-void create_output_file(const std::vector<ojson>& features_buffer, const std::filesystem::path& outfilepath, int file_number) {
-    const auto filename = outfilepath / fmt::format("output_{}.geojson", file_number);
-
-    std::ofstream output_file(filename);
-    json_stream_encoder encoder(output_file);
-
-    encoder.begin_object();
-    encoder.key("type");
-    encoder.string_value("FeatureCollection");
-    encoder.key("features");
-    encoder.begin_array();
-
-    //for (const auto& feature : features_buffer | std::views::reverse) {
-    for (const auto& feature : features_buffer) {
-        feature.dump(encoder);
-
-        if (feature.contains("geometry") && feature["geometry"].contains("coordinates")) {
-
-            const auto& coords = feature["geometry"]["coordinates"];
-
-            if (coords.is_array() && !coords.empty()) {
-                try {
-                    const auto points = coords[0].as<std::vector<PyramidTiffData::Point2D>>();
-
-                    for (const auto& pt : points) {
-                        fmt::println("Points: {}, {}", pt.x, pt.y);
-                    }
-                }
-                catch (const std::exception& e) {
-                    std::cerr << "Error parsing coordinates: " << e.what() << std::endl;
-                }
-            }
-        }
-    }
-
-    encoder.end_array();
-    encoder.end_object();
-    encoder.flush();
-
-    output_file.close();
-
-    fmt::println("Created {} with {} features", filename, features_buffer.size());
-}
-
 namespace utils
 {
     static std::filesystem::path change_extension(
@@ -94,30 +50,54 @@ namespace utils
         return result;
     }
 
-}
+    static void create_output_file(const std::vector<ojson>& features_buffer, const std::filesystem::path& outfilepath, int file_number) {
+        const auto filename = outfilepath / fmt::format("output_{}.geojson", file_number);
 
-// Reads a tiff file that contains an image pyramid
-// and writes each chanel of the hightest level to disk
-int main(int argc, char* argv[]) {
+        std::ofstream output_file(filename);
+        json_stream_encoder encoder(output_file);
 
-    if (argc != 2) {
-        fmt::println("Usage:\n    tifftest <path_to_tiff_file>\nYou did not provide a tiff file.");
-        return 1;
+        encoder.begin_object();
+        encoder.key("type");
+        encoder.string_value("FeatureCollection");
+        encoder.key("features");
+        encoder.begin_array();
+
+        for (const auto& feature : features_buffer) {
+            feature.dump(encoder);
+
+            if (feature.contains("geometry") && feature["geometry"].contains("coordinates")) {
+
+                const auto& coords = feature["geometry"]["coordinates"];
+
+                if (coords.is_array() && !coords.empty()) {
+                    try {
+                        const auto points = coords[0].as<std::vector<PyramidTiffData::Point2D>>();
+                    }
+                    catch (const std::exception& e) {
+                        std::cerr << "Error parsing coordinates: " << e.what() << std::endl;
+                    }
+                }
+            }
+        }
+
+        encoder.end_array();
+        encoder.end_object();
+        encoder.flush();
+
+        output_file.close();
+
+        fmt::println("Created {} with {} features", filename, features_buffer.size());
     }
-	
-    const std::filesystem::path img_path = argv[1];
-    const std::filesystem::path json_path = utils::change_extension(img_path, ".geojson");
-    fmt::println("Reading file: {}", img_path);
-    fmt::println("JSON file: {}", json_path);
 
-	try {
+    static void split_json_file(const std::filesystem::path& json_path)
+    {
         std::ifstream input_file(json_path);
 
         json_stream_cursor cursor(input_file);
         json_decoder<ojson> decoder;
 
         std::vector<ojson> features_buffer;
-        const int MAX_FEATURES_PER_FILE = 10;
+        constexpr int MAX_FEATURES_PER_FILE = 100;
         int file_number = 1;
         bool in_features_array = false;
 
@@ -150,7 +130,7 @@ int main(int argc, char* argv[]) {
 
                     // When buffer reaches 10 features, write to file and reset
                     if (features_buffer.size() >= MAX_FEATURES_PER_FILE) {
-                        create_output_file(features_buffer, json_path.parent_path(), file_number);
+                        utils::create_output_file(features_buffer, json_path.parent_path(), file_number);
                         features_buffer.clear();
                         file_number++;
                     }
@@ -163,7 +143,7 @@ int main(int argc, char* argv[]) {
                 if (in_features_array) {
                     // Write remaining features to a file at the end
                     if (!features_buffer.empty()) {
-                        create_output_file(features_buffer, json_path.parent_path(), file_number);
+                        utils::create_output_file(features_buffer, json_path.parent_path(), file_number);
                         features_buffer.clear();
                     }
                     in_features_array = false;
@@ -180,18 +160,13 @@ int main(int argc, char* argv[]) {
 
         input_file.close();
 
-        fmt::println("Finished");
+    }
 
-	}
-	catch (const std::exception& e) {
-		fmt::println("Error: {}", e.what());
-		return 1;
-	}
-
-	try {
+    static void copy_tiff_file(const std::filesystem::path& img_path, const std::filesystem::path& json_path)
+    {
         // read a JSON file
         std::ifstream loadFile(json_path, std::ios::in);
-        if (!loadFile.is_open()) return 1;
+        if (!loadFile.is_open()) return;
 
         // read image
         const PyramidTiffData::OmeTiffPyramid tiffReader(img_path);
@@ -207,6 +182,28 @@ int main(int argc, char* argv[]) {
         const PyramidTiffData::Image single_level = tiffReader.read_level(current_series, current_channel);
 
         PyramidTiffData::write_to_disk_as_single_page_tiffs(single_level, fmt::format("./output_channels_level_{}", current_channel));
+
+    }
+
+}
+
+// Reads a tiff file that contains an image pyramid
+// and writes each chanel of the hightest level to disk
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        fmt::println("Usage:\n    tifftest <path_to_tiff_file>\nYou did not provide a tiff file.");
+        return 1;
+    }
+	
+    const std::filesystem::path img_path = argv[1];
+    const std::filesystem::path json_path = utils::change_extension(img_path, ".geojson");
+    fmt::println("Reading file: {}", img_path);
+    fmt::println("JSON file: {}", json_path);
+
+	try {
+        utils::copy_tiff_file(img_path, json_path);
+        utils::split_json_file(json_path);
     }
     catch (const std::exception& e) {
         fmt::println("Error: {}", e.what());
